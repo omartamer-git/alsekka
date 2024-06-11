ebc9aadd70e0a36fddc64fd7d7b1053a4e767adb
omartamer-git <omartamer02@hotmail.com>
2024-06-11 05:49:11 +0300

    Babel undo WDYR

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: ['react-native-reanimated/plugin',],
-  env: {
-    development: {
-      plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'classic' }]],
-    },
-  },
+  // env: {
+  //   development: {
+  //     plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'classic' }]],
+  //   },
+  // },
 };
