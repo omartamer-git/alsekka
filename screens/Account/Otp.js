cf97eed9887db6493defbde0eb2aa4731ddd5c66
omartamer-git <omartamer02@hotmail.com>
2024-06-21 15:19:06 +0300

    latest

--- screens/Account/Otp.js
+++ screens/Account/Otp.js
@@ -5,6 +5,7 @@ import {
     Linking,
     Platform,
     ScrollView,
+    StyleSheet,
     Text,
     TextInput,
     View
@@ -27,12 +28,18 @@ function Otp({ route, navigation }) {
     const gender = route.params?.gender;
     const onVerify = route.params?.onVerify;
     const [currentInput, setCurrentInput] = useState(0);
-    const numDigits = 4;
     const [error, setError] = useState(null);
     let countdownInterval = null;
     const { getOtp, sendOtp, sendOtpSecurity, isVerified, createAccount, login } = useUserStore();
     const [uri, setUri] = useState('');
     const [token, setToken] = useState('');
+
+    const otpInputs = [useRef(), useRef(), useRef(), useRef(), useRef()]
+    const [otpValue0, setOtpValue0] = useState("");
+    const [otpValue1, setOtpValue1] = useState("");
+    const [otpValue2, setOtpValue2] = useState("");
+    const [otpValue3, setOtpValue3] = useState("");
+    const [otpValue4, setOtpValue4] = useState("");
 
     const clockTick = function () {
         isVerified(phone).then(response => {
@@ -108,6 +115,40 @@ function Otp({ route, navigation }) {
         };
     }, []);
 
+    function setOtpValue(index, text) {
+        if (index === 0) {
+            setOtpValue0(text);
+        } else if (index === 1) {
+            setOtpValue1(text);
+        } else if (index === 2) {
+            setOtpValue2(text);
+        } else if (index === 3) {
+            setOtpValue3(text);
+        } else {
+            setOtpValue4(text);
+        }
+    }
+
+    function changeOTP(index, text) {
+        setOtpValue(index, text);
+
+        if ((index === 0 && text.length === 0) || (index === 4 && text.length === 1)) {
+            return;
+        }
+
+        if (text.length === 0) {
+            otpInputs[index - 1].current.focus();
+        } else {
+            otpInputs[index + 1].current.focus();
+        }
+    }
+
+    function handleKeyPress(index, { nativeEvent }) {
+        if (nativeEvent.key === 'Backspace') {
+            otpInputs[index - 1].current.focus();
+        }
+    }
+
     useFocusEffect(onFocusEffect); // register callback to focus events    
 
     const { t } = useTranslation();
@@ -125,16 +166,41 @@ function Otp({ route, navigation }) {
                     {t('code_sent')}
                 </Text>
             </View>
-            <View style={[styles.w100, styles.bgPrimary, { height: 48 }]}>
+            {/* <View style={[styles.w100, styles.bgPrimary, { height: 48 }]}>
                 <View style={[styles.w100, styles.bgLightGray, styles.h100, { borderTopLeftRadius: 16, borderTopRightRadius: 16 }]} />
-            </View>
-            <ScrollView keyboardShouldPersistTaps={'handled'} style={styles.flexOne} contentContainerStyle={[containerStyle]}>
-                <Button disabled={!uri} onPress={openWhatsapp} bgColor={palette.success} textColor={palette.white} text="Verify Using WhatsApp" />
+            </View> */}
+            <View style={[styles.flexOne, styles.w100, styles.bgPrimary]}>
+                <View style={[styles.flexOne, styles.w100, styles.defaultPadding, styles.bgLightGray, styles.fullCenter, { borderTopLeftRadius: 16, borderTopRightRadius: 16 }]}>
 
-                <FastImage source={{uri: 'https://seaats.app/img/create_acc_guide.png'}} style={{width: '100%', aspectRatio: 1, borderRadius: 8}}/>
-            </ScrollView>
+                    <View style={[styles.flexRow, styles.w100, styles.gap10]}>
+                        <TextInput style={OTPStyles.inputStyle} value={otpValue0} onChangeText={(text) => changeOTP(0, text)} keyboardType='number-pad' maxLength={1} ref={otpInputs[0]}></TextInput>
+                        <TextInput onKeyPress={(e) => handleKeyPress(1, e)} style={OTPStyles.inputStyle} value={otpValue1} onChangeText={(text) => changeOTP(1, text)} keyboardType='number-pad' maxLength={1} ref={otpInputs[1]}></TextInput>
+                        <TextInput onKeyPress={(e) => handleKeyPress(2, e)} style={OTPStyles.inputStyle} value={otpValue2} onChangeText={(text) => changeOTP(2, text)} keyboardType='number-pad' maxLength={1} ref={otpInputs[2]}></TextInput>
+                        <TextInput onKeyPress={(e) => handleKeyPress(3, e)} style={OTPStyles.inputStyle} value={otpValue3} onChangeText={(text) => changeOTP(3, text)} keyboardType='number-pad' maxLength={1} ref={otpInputs[3]}></TextInput>
+                        <TextInput onKeyPress={(e) => handleKeyPress(4, e)} style={OTPStyles.inputStyle} value={otpValue4} onChangeText={(text) => changeOTP(4, text)} keyboardType='number-pad' maxLength={1} ref={otpInputs[4]}></TextInput>
+                    </View>
+
+                    <Button disabled={!uri} onPress={openWhatsapp} bgColor={palette.accent} textColor={palette.white} text={t('verify_phone')} />
+                    {/* <FastImage source={{ uri: 'https://seaats.app/img/create_acc_guide.png' }} style={{ width: '100%', aspectRatio: 1, borderRadius: 8 }} /> */}
+                </View>
+            </View>
         </ScreenWrapper>
     );
 };
+
+const OTPStyles = StyleSheet.create({
+    inputStyle: {
+        ...styles.flexOne,
+        ...styles.border1,
+        ...styles.br8,
+        ...styles.borderLight,
+        ...styles.text,
+        ...styles.textCenter,
+        ...styles.font28,
+        ...styles.p8,
+        aspectRatio: 1,
+        lineHeight: null
+    }
+})
 
 export default Otp;
