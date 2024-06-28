2bd2cbf5bbd2285861e7f0eeb33ab10bd75aeeda
omartamer-git <omartamer02@hotmail.com>
2024-06-28 14:39:10 +0300

    Latest V1.1.0

--- components/RoundedButton.js
+++ components/RoundedButton.js
@@ -1,10 +1,24 @@
 import { Text, TouchableOpacity } from "react-native";
 import { styles, palette } from "../helper";
 
-export default function RoundedButton({icon, active, children, onPress, dark, activeColor, activeBgColor, textStyle=[], style}) {
+export default function RoundedButton({ icon, active, children, onPress, dark, activeColor, activeBgColor, textStyle = [], style }) {
     return (
         <>
-            <TouchableOpacity onPress={onPress} activeOpacity={0.8} style={[styles.brFull, styles.flexRow, styles.gap10, styles.ph24, styles.pv8, styles.fullCenter, ...style, { backgroundColor: active ? activeBgColor : (style.backgroundColor || palette.lighter), borderWidth: 2, borderColor: active ? (activeColor || palette.accent) : 'transparent' }]}>
+            <TouchableOpacity
+                onPress={onPress}
+                activeOpacity={0.8}
+                style={[
+                    styles.brFull,
+                    styles.flexRow,
+                    styles.gap10,
+                    styles.ph24,
+                    styles.pv8,
+                    styles.fullCenter,
+                    ...style,
+                    active && activeBgColor ? { backgroundColor: activeBgColor } : {},
+                    { borderWidth: 2, borderColor: active ? (activeColor || palette.accent) : 'transparent' }
+                ]}
+            >
                 {
                     icon &&
                     icon
@@ -12,7 +26,7 @@ export default function RoundedButton({icon, active, children, onPress, dark, ac
                 <Text style={[styles.text, styles.semiBold, styles.textCenter, { color: active && activeColor ? activeColor : (dark ? palette.white : palette.dark) }, ...textStyle]}>
                     {children}
                 </Text>
-            </TouchableOpacity>
+            </TouchableOpacity >
         </>
     )
 }
